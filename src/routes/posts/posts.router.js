dfa65af001c5842c90dd63c4bafd3dbf60b07443
Berat Bozkurt <beratbozkurt1999@gmail.com>
2019-08-13 23:04:21 +0300

    register

--- src/routes/posts/posts.router.js
+++ src/routes/posts/posts.router.js
@@ -29,7 +29,7 @@ const route = () => {
 
 
     let kimmis = Users.findOne({ _id: who }, function (err, doc) {
-      return doc.toJSON();
+      console.log(doc);
     });
     console.log(kimmis)
     const newPost = new Posts({
